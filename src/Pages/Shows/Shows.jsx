f06e1e1baf60237b10fb4b6836f95763cce1747e
Showrab15 <supto50showrab@gmail.com>
2023-06-07 04:41:19 +0600

    extra route added for booked show

--- src/Pages/Shows/Shows.jsx
+++ src/Pages/Shows/Shows.jsx
@@ -31,7 +31,7 @@ const Summary = () => {
 
   return (
     <div>
-      <h3 className="display-4 text-center mt-5 fw-semibold">Tops Shows Of The world </h3>
+      <h3 className="display-4 text-center mt-5 fw-semibold">Top Shows Of The World </h3>
       <div className=" container mx-auto mt-4 row row-cols-1 row-cols-md-3 g-4">
 
         {data.map(show =>
